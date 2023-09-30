a9d9aaff3622c5bdad930e318012faba84e5712b
nazmul906 <nazmulhb96@gmail.com>
2023-09-30 15:27:59 +0600

    api for user enrollment in a course is created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+const { ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -71,6 +72,55 @@ async function run() {
       res.send(result);
     });
 
+    // todo:enrolled
+    // if enrollment field not exist in that course add it in that course and the user email
+    // update the course a new field enrolled with the email, when new user enroll add that name in that enrollment filed as aray
+
+    app.post("/enroll/:id", async (req, res) => {
+      const courseId = req.params.id; // Get the course ID from the URL
+      const userEmail = req.body.email; // Get the user's email from the request body
+
+      try {
+        // Check if the course with the given ID exists
+        const course = await classCollection.findOne({
+          _id: new ObjectId(courseId),
+        });
+
+        if (!course) {
+          return res.status(404).json({ message: "Course not found" });
+        }
+
+        // Check if the course has an "enrollment" field, if not, create it as an array
+        if (!course.enrollment) {
+          course.enrollment = [];
+        }
+
+        // Check if the user's email is already in the "enrollment" array
+        if (course.enrollment.includes(userEmail)) {
+          return res
+            .status(400)
+            .json({ message: "User already enrolled in this course" });
+        }
+
+        // Add the user's email to the "enrollment" array
+        course.enrollment.push(userEmail);
+
+        // Update the course document with the new "enrollment" data
+        const result = await classCollection.updateOne(
+          { _id: new ObjectId(courseId) },
+          { $set: { enrollment: course.enrollment } }
+        );
+        console.log("updated enrollment", result);
+        if (result.modifiedCount === 1) {
+          return res.status(200).json({ message: "Enrolled successfully" });
+        } else {
+          return res.status(500).json({ message: "Enrollment failed" });
+        }
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
